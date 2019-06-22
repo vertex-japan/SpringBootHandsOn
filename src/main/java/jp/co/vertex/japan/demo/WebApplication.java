9a8705769ca0855950d9b122f966fe4a98a2e0a1
kijin <kijin@DESKTOP-6JPMU6S>
2019-06-22 09:59:59 +0900

    demo

---
+++ src/main/java/jp/co/vertex/japan/demo/WebApplication.java
@@ -0,0 +1,13 @@
+package jp.co.vertex.japan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
